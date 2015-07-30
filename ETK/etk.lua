b8153d8cb97e6ee87c6dc6c410431ae273dca914
Adrien Bertrand <bertrand.adrien@gmail.com>
2015-07-30 19:12:11 -0400

    ETK overall loader

---
+++ ETK/etk.lua
@@ -0,0 +1,17 @@
+----------------
+-- ETK Loader --
+----------------
+
+etk = {}
+
+do
+    local etk = etk
+    
+    --include "helpers.lua"
+    --include "graphics.lua"
+    --include "screenmanager.lua"
+    --include "viewmanager.lua"
+    --include "eventmanager.lua"
+    --include "widgetmanager.lua"
+    --include "dialog.lua"
+end
